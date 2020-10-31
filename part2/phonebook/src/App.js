467aa180a0006b73c0ac57f634f3aedd359a910f
gulzar <ali.gulzar@tuni.fi>
2020-10-31 17:50:28 +0500

    Part 2 adding styles to react app exercises done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import contacts from './services/contacts';
 
+const Notification = ({message}) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="message" style={{display: message ? 'block' : 'none'}}>
+      {message}
+    </div>
+  )
+}
+
 const Filter = ({onChangeAction, value}) => {
   return (
     <form>
@@ -45,6 +57,7 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ filter, setFilter ] = useState('')
+  const [ message, setMessage ] = useState(null)
 
   useEffect(() => {
     contacts.getAll().then((response) => setPersons(response.data))
@@ -56,13 +69,21 @@ const App = () => {
 
     if (!names.includes(newName)) {
       contacts.create({name: newName, number: newNumber}).then(response => {
+        setMessage(`${newName} added in the phonebook`)
         setPersons([...persons].concat(response.data))
+        setTimeout(() => {
+          setMessage(null)
+        }, 2000)
       })
     } else {
       if (window.confirm(`${newName} already exists in the phonebook, replace the old number with the new one?`)) {
         const existingPerson = persons.find(person => person.name === newName)
         contacts.update(existingPerson.id, {...existingPerson, number: newNumber}).then(response => {
+          setMessage(`${newName} person contact number is updated.`)
           setPersons(persons.map(person => (person.id === existingPerson.id && person.number !== newNumber) ? response.data : person))
+          setTimeout(() => {
+            setMessage(null)
+          }, 2000)
         })
       }
     }
@@ -71,7 +92,11 @@ const App = () => {
   }
 
   const handleDelete = (id) => {
+    setMessage(`Person with id ${id} is deleted from the contacts.`)
     contacts.deleteContact(id).then(setPersons(persons.filter(person => person.id !== id)))
+    setTimeout(() => {
+      setMessage(null)
+    }, 2000)
   }
 
   const filterPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
@@ -88,6 +113,7 @@ const App = () => {
 
   return (
     <div>
+      <Notification message={message}/>
       <h2>Phonebook</h2>
       <Filter onChangeAction={onChangeAction} value={filter}/>
       <h2>add a new</h2>
